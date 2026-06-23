34760b87839b97930d04eeb7368fbbceddf6c320
kisito10 <kisitoengono@gmail.com>
2026-06-24 00:21:15 +0100

    prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production:         true,
+  imgBase:            'https://microservicio-noticias.onrender.com',
+  apiUserUrl:         'https://microservicios-users.onrender.com/api/user',
+  apiSolicitudUrl:    'https://microservicios-users.onrender.com/api/solicitudes',
+  apiEdificioUrl:     'https://microservicios-users.onrender.com/api/edificio',
+  apiHabitacionUrl:   'https://microservicios-users.onrender.com/api/habitacion',
+  apiSeccionUrl:      'https://microservicio-secciones.onrender.com/api/seccion',
+  apiNoticiaUrl:      'https://microservicio-noticias.onrender.com/api/noticia',
+  apiLecturaUrl:      'https://microservicio-noticias.onrender.com/api/lectura',
+  apiDestinatarioUrl: 'https://microservicio-noticias.onrender.com/api/destinatario',
+};
